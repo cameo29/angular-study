e1f5c9caea42344f6ae3f75392d86018b3d07087
jaein <qkdwodls29@gmail.com>
2017-12-01 16:49:19 +0900

    파이프전

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+interface User { 
+  id: number; 
+  name: string 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  users: User[] = [
+    { id: 1, name: 'Lee' },
+    { id: 2, name: 'Kim' },
+    { id: 3, name: 'Baek' }
+  ];
+
+  // user를 추가한다
+  addUser(name: string) {
+    if (name) {
+      this.users.push({ id: this.getNewUserId(), name: name });
+    }
+  }
+
+  // 해당 user를 제거한다.
+  removeUser(userid: number) {
+    this.users = this.users.filter(({ id }) => id !== userid);
+  }
+
+  // 추가될 users의 id를 반환한다
+  getNewUserId(): number {
+    return this.users.length ? Math.max(...this.users.map(({ id }) => id)) + 1 : 1;
+  }
+  // 변경 트래킹 기준을 반환한다.
+  trackByUserId(index: number, user: User) {
+    // user.id를 기준으로 변경을 트래킹한다.
+    return user.id; // or index
+  }
 }
